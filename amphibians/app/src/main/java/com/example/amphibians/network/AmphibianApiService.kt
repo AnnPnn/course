4b99fcdfa5c6a78e010fd1839fae43b067285bf3
AnnPnn <anikeevapolina0@gmail.com>
2022-04-26 15:36:22 +0300

    Amphibians
    Amphibians

---
+++ amphibians/app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.amphibians.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+
+// TODO: Создайте свойство для базового URL-адреса, указанного в codelab
+private const val BASE_URL =
+"https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
+
+// TODO: Создайте объект Moshi с помощью Kotlin adapter factory, который Retrofit будет использовать для анализа JSON
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+
+// TODO: Создайте модифицированный объект с помощью преобразователя Moshi
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface AmphibianApiService {
+    // TODO: Объявите suspended function, чтобы получить список амфибий
+    @GET("android-basics-kotlin-unit-4-pathway-2-project-api.json")
+    suspend fun getAmphibians(): List<Amphibian>
+}
+
+// TODO: Создайте объект, который предоставляет службу модернизации с отложенной инициализацией
+object AmphibianApi {
+
+    val retrofitService : AmphibianApiService by lazy {
+        retrofit.create(AmphibianApiService::class.java)
+    }
+
+}
+
